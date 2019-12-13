ad867f765d2a4172f18aba3362d8dc5b0bd23071
Chris Anderson <cda84097@gmail.com>
2019-12-13 16:02:57 -0700

    Deal-ViewController Without Implementation New

--- Deal-Cards/Deal-Cards/DealViewController.swift
+++ Deal-Cards/Deal-Cards/DealViewController.swift
@@ -9,11 +9,54 @@
 import UIKit
 
 class DealViewController: UIViewController {
-
+    
+    // MARK: - Outlets
+    
+    
+    // MARK: - Life Cycle Methods
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        designDealButton()
     }
-
-
+    
+    // MARK: - Actions
+    
+    @IBAction func dealButtonPressed(_ sender: Any) {
+        CardController.fetchCard { (result) in
+            DispatchQueue.main.async {
+                switch result {
+                case .success(let card):
+                    self.fetchImageAndUpdateUI(for: card)
+                case .failure(let error):
+                    print(error.localizedDescription)
+                }
+            }
+        }
+        
+    }
+    
+    // MARK: - Custom Methods
+    
+    func fetchImageAndUpdateUI(for card: Card) {
+        CardController.fetchImage(for: card) { (result) in
+            DispatchQueue.main.async {
+                switch result {
+                    case .success(let image):
+                        self.cardLabel.text = card.value + " OF " + card.suit
+                        self.cardImage = image
+                case .failure(let error):
+                    print(error.localizedDescription)
+                }
+            }
+        }
+    }
+    
+    func designDealButton() {
+        buttonName.layer.borderWidth = 2
+        buttonName.setTitle("Deal", for: .normal)
+        buttonName.layer.cornerRadius = buttonName.frame.height / 2
+    }
+    
 }
 
